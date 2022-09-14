9d3777f40b37d49725a78860dc49024a0d460aaa
git-daun <94218285+git-daun@users.noreply.github.com>
2022-09-14 16:21:22 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Table from "./component/Table";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>버튼상태 state로 관리하기</h1>
+      <Table />
+    </div>
+  );
+}
